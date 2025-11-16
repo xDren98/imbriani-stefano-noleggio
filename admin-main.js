e3da5946d97f05bfa09de802127dced9a5347e8b
xDren98 <dreenhd@gmail.com>
2025-11-16 18:12:21 +0100

    🗑️ Svuotamento main

--- admin-main.js
+++
@@ -1,8 +0,0 @@
-import './admin-validation.js';
-import './admin-api.js';
-import './admin-store.js';
-import './admin-renderer.js';
-import './admin-ui.js';
-import './admin-orchestrator.js';
-
-export {};
